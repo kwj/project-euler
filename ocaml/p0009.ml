cb27382ba185fd84778bfbb2b1f89c5f6f08fc72
kwj <kwj@vlax.net>
2021-10-29 18:16:56 -0700

    problem 9 (ocaml)

---
+++ ocaml/p0009.ml
@@ -0,0 +1,22 @@
+(* Project Euler: Problem 9 *)
+
+exception Failure of string
+
+let rec find_triplet a b c =
+  if a > 332 then
+    raise (Failure "no answer")
+  else
+    if b > 998 then
+      find_triplet (a + 1) (a + 2) (a + 3)
+    else
+      if c > 999 then
+        find_triplet a (b + 1) (b + 2)
+      else
+        if a + b + c = 1000 && (a * a) + (b * b) = (c * c) then
+          (a, b, c)
+        else
+          find_triplet a b (c + 1)
+
+let () =
+  let a, b, c = find_triplet 1 2 3 in
+  Printf.printf "Pythagorean triplet for which a + b + c = 1000 is (%d, %d, %d).\na*b*c = %d\n" a b c (a * b * c);;
