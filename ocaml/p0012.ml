26928b8ed47cfdfcfd636ff865ba222948678f8c
kwj <kwj@vlax.net>
2021-11-02 07:51:29 -0700

    problem 12 (ocaml)

---
+++ ocaml/p0012.ml
@@ -0,0 +1,28 @@
+(* Project Euler: Problem 12 *)
+
+let divisors n =
+  let u_limit = truncate(sqrt (float n)) in
+  let rec aux lst i =
+    if i > u_limit then
+      lst
+    else
+      if n mod i <> 0 then
+        aux lst (succ i)
+      else
+        if i * i = n then
+          aux (i::lst) (succ i)
+        else
+          aux ((n / i) :: (i::lst)) (succ i)
+  in
+  aux [] 1
+                          
+let rec find_num a b =
+  let tri_num = a + b in
+  let divs = divisors tri_num in
+  if List.length divs >= 500 then
+    tri_num
+  else
+    find_num tri_num (succ b)
+
+let () =
+  Printf.printf "the value of the first triangle number to have over five hundred divisors is %d\n" (find_num 0 1)
