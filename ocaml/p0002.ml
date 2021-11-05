1edaf23dce0e668eedfce7021a670e41c2e2c565
kwj <kwj@vlax.net>
2021-11-05 07:54:27 -0700

    use List.fold_left

--- ocaml/p0002.ml
+++ ocaml/p0002.ml
@@ -6,11 +6,6 @@ let rec make_fib_seq upper l =
   | m :: n :: _ -> if m + n >= upper then l else make_fib_seq upper (m + n :: l)
   | _ -> l;;
 
-let rec sum_of_seq l =
-  match l with
-  | [] -> 0
-  | h :: t -> h + sum_of_seq t;;
-
 let () =
   Printf.printf "sum of the even-valued terms of the Fibonacci sequence is %d\n"
-    (sum_of_seq (List.filter (fun n -> n mod 2 = 0) (make_fib_seq 4_000_000 [])));;
+    (List.fold_left (+) 0 (List.filter (fun n -> n mod 2 = 0) (make_fib_seq 4_000_000 [])))
