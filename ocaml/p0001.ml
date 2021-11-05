8ef0b8e8acde11d9b83fefb02559f667a2beacb8
kwj <kwj@vlax.net>
2021-11-05 07:37:32 -0700

    use List.fold_left

--- ocaml/p0001.ml
+++ ocaml/p0001.ml
@@ -3,10 +3,5 @@
 let make_seq upper =
   List.filter (fun n -> n mod 3 = 0 || n mod 5 = 0) (List.init upper (fun n -> n));;
 
-let rec sum_of_seq l =
-  match l with
-  | [] -> 0
-  | h :: rest -> h + (sum_of_seq rest);;
-
 let () =
-  Printf.printf "sum of all the multiples of 3 or 5 below 1000 is %d\n" (sum_of_seq (make_seq 1000));;
+  Printf.printf "sum of all the multiples of 3 or 5 below 1000 is %d\n" (List.fold_left (+) 0 (make_seq 1000))
