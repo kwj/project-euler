2a02588a5e0924c079c8a94c26c94d5fff78938e
kwj <kwj@vlax.net>
2021-11-18 12:06:11 -0800

    problem 28 (ocaml)

---
+++ ocaml/p0028.ml
@@ -0,0 +1,34 @@
+(* Project Euler: Problem 28 *)
+
+(*
+  21 22 23 24 25
+  20  7  8  9 10
+  19  6  1  2 11
+  18  5  4  3 12
+  17 16 15 14 13
+         |  |  |  |
+      (n=0, 1, 2, 3, ...)
+
+  the upper right number is:
+    1    [n=0]
+    (2n+1)**2    [n=>1]
+
+  so, the sum of numbers in the four corners is:
+    (2n+1)**2 + ((2n+1)**2 - 2n) + ((2n+1)**2 - 4n) + ((2n+1)**2 - 6n)
+      = 16n**2 + 4n + 4   [n>=1]
+
+  Answer: 1 + sum_{n=1}^{(1001-1)/2} (16n**2 + 4n + 4)
+
+ *)
+
+let solve side_len =
+  let rec aux n result =
+    if n < 1 then
+      (succ result)
+    else
+      aux (pred n) (result + (16 * n * n + 4 * n + 4))
+  in
+  aux ((side_len - 1) / 2) 0
+
+let () =
+  Printf.printf "the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed is %d\n" (solve 1001)
