f14cf6f6224f709c07ab6a869ca3cb95898856a5
kwj <kwj@vlax.net>
2022-12-24 00:14:36 -0800

    Add problem #1

---
+++ python/euler/problems/p0001.py
@@ -0,0 +1,17 @@
+
+# project euler: problem 1
+
+from time import perf_counter
+
+def sum_of_divs_by(n, upper):
+    return (n + (upper - (upper % n))) * (upper // n) // 2
+
+def compute(ulimit):
+    return sum_of_divs_by(3, ulimit) + sum_of_divs_by(5, ulimit) - sum_of_divs_by(15, ulimit)
+    
+def solve():
+    start = perf_counter()
+    result = compute(1000 - 1)
+    elapsed_time = perf_counter() - start
+
+    return (result, "{:f}".format(elapsed_time))
