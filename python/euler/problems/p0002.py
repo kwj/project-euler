94865e110b3a76b283a9c9c8c7311c44422a02c5
kwj <kwj@vlax.net>
2022-12-26 16:44:55 -0800

    Add a solution to problem #2

---
+++ python/euler/problems/p0002.py
@@ -0,0 +1,21 @@
+
+# project euler: problem 2
+
+from itertools import takewhile
+from time import perf_counter
+
+def fib_gen():
+    a, b = 0, 1
+    while True:
+        a, b = a + b, a
+        yield a
+
+def compute(ulimit):
+    return sum(i for i in takewhile(lambda n: n < ulimit, fib_gen()) if i % 2 == 0)
+
+def solve():
+    start = perf_counter()
+    result = compute(4_000_000)
+    elapsed_time = perf_counter() - start
+
+    return (result, "{:f}".format(elapsed_time))
