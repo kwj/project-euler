c18dca35efa32f6742a514f3084cf6a1d0bc1a7b
kwj <kwj@vlax.net>
2022-12-26 16:28:34 -0800

    Make clear that List.fold() is used to calculate sum

--- ocaml/bin/p0010.ml
+++ ocaml/bin/p0010.ml
@@ -5,6 +5,6 @@ open Core
 let exec () =
   let module E = Euler.Eratosthenes in
   let prime_t = E.generate 2_000_000 in
-  Int.to_string (List.fold ~init:0 ~f:(fun acc n -> acc + n) (E.to_list prime_t))
+  Int.to_string (List.fold ~init:0 ~f:(+) (E.to_list prime_t))
 
 let () = Euler.Task.run exec
