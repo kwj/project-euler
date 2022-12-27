6b202a5dfda55a17e2a0f42e33cc55e1dad32e3f
kwj <kwj@vlax.net>
2022-12-26 23:40:40 -0800

    Make to return a sorted list

--- python/euler/lib/prime/sieve.py
+++ python/euler/lib/prime/sieve.py
@@ -1,5 +1,37 @@
 
 class Sieve:
+    '''Prime number flag/Least prime factors/Mobius function tables
+
+    Compute above sequences in a range of less than or equal to a specific value.
+    Note: The first element is an invalid value.
+
+      public attributes:
+        prime_tbl[]: bool
+          [False, False, True, True, False, True, False, True, False, ...]
+             *      1      2     3     4      5     6      7     8
+        minfactor_tbl[]: int
+          [-1, 1, 2, 3, 2, 5, 2, 7, 2, ...]
+            *  1  2  3  4  5  6  7  8
+        mobius_tbl[]: int
+          [1, 1, -1, -1, 0, -1, 1, -1, 0, ...]
+           *  1   2   3  4   5  6   7  8
+
+      public methods:
+        get_primes()
+        is_prime()
+        factorize()
+        divisors()
+
+        >>> pt = sieve.sieve(100)
+        >>> pt.get_primes()
+        [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+        >>> pt.is_prime(87)
+        False
+        >>> pt.factorize(84)
+        [(2, 2), (3, 1), (7, 1)]
+        >>> pt.divisors(84)
+        [1, 2, 3, 4, 6, 7, 12, 14, 21, 28, 42, 84]
+    '''
     def __init__(self, num):
         self.prime_tbl = [True] * (num + 1)
         self.minfactor_tbl = [-1] * (num + 1)
@@ -15,6 +47,7 @@ class Sieve:
             self.minfactor_tbl[i] = i
             self.mobius_tbl[i] = -1
 
+            # Not 'i * i' for start value since caliculate mobius function
             for j in range(i * 2, num + 1, i):
                 self.prime_tbl[j] = False
                 if self.minfactor_tbl[j] == -1:
@@ -65,7 +98,7 @@ class Sieve:
                     v *= base
                     result.append(result[i] * v)
 
-        return result
+        return sorted(result)
 
 def sieve(num):
     return Sieve(num)
