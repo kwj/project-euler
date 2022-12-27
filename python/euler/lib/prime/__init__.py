418f1be2de449a98e76c3688bc52e510a88bf791
kwj <kwj@vlax.net>
2022-12-26 23:41:21 -0800

    Add get_primes() and get_prime_tbl()

--- python/euler/lib/prime/__init__.py
+++ python/euler/lib/prime/__init__.py
@@ -1,5 +1,6 @@
 
 from .isprime import is_prime, is_probably_prime
 from .sieve import sieve
+from .simple_sieve import get_primes, get_prime_tbl
 from .generator import generator
 from .eratosthenes import eratosthenes
---
+++ python/euler/lib/prime/simple_sieve.py
@@ -0,0 +1,30 @@
+
+def get_primes(limit):
+    '''Returns a sequence of prime numbers in a range of less than or equal to a specific value.
+
+    >>> get_primes(100)
+    [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    '''
+    primes = list(range(limit + 1))
+    primes[4::2] = [0] * len(primes[4::2])
+    for i in range(3, limit + 1, 2):
+        if primes[i] != 0:
+            primes[i * i::i] = [0] * len(primes[i * i::i])
+
+    return [x for x in primes if x > 1]
+
+def get_prime_tbl(limit):
+    '''Returns a table for prime number determination
+
+    >>>list(enumerate(get_prime_tbl(20)))
+    [(0, False), (1, False), (2, True), (3, True), (4, False), (5, True), (6, False),
+     (7, True), (8, False), (9, False), (10, False), (11, True), (12, False), (13, True),
+     (14, False), (15, False), (16, False), (17, True), (18, False), (19, True), (20, False)]
+    '''
+    prime_tbl = [False] * 2 + [True] * (limit - 1)
+    prime_tbl[4::2] = [False] * len(prime_tbl[4::2])
+    for i in range(3, limit + 1, 2):
+        if prime_tbl[i] == True:
+            prime_tbl[i * i::i] = [False] * len(prime_tbl[i * i::i])
+
+    return prime_tbl
