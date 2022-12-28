051ee2e292e3a00f6d1e63f1a95f184d6beccfa8
kwj <kwj@vlax.net>
2022-12-27 16:01:24 -0800

    Improve performance using another algorithm

--- ocaml/bin/p0032.ml
+++ ocaml/bin/p0032.ml
@@ -12,24 +12,34 @@
 
 open Core
 
-let solve () =
-  let module U = Euler.Util in
-  (* 9-digits -> [(4-digits, 1-digit), 4-digits; (3-digits, 2-digits), 4-digits] *)
-  let make_tpls lst =
-    let lhs, rhs = List.split_n lst 5 in
-    [List.split_n lhs 4, rhs; List.split_n lhs 3, rhs]
-  in
-  let confirm_requirement ((m1, m2), prod) =
-    U.list_to_num m1 * U.list_to_num m2 = U.list_to_num prod
-  in
+let make_case_1 () =
+  let lst = ref [] in
+  for m1 = 1_000 to 9_999 do
+    for m2 = 2 to 9 do
+      let prod = m1 * m2 in
+      if prod < 10_000 then
+        lst := (m1 * (Int.pow 10 5) + m2 * (Int.pow 10 4) + prod, prod) :: !lst
+    done
+  done;
+  !lst
 
-  Euler.Util.permutation 9 [1; 2; 3; 4; 5; 6; 7; 8; 9]
-  |> List.map ~f:make_tpls
-  |> List.concat
-  |> List.filter ~f:confirm_requirement
-  |> List.map ~f:(fun (_, p) -> U.list_to_num p)
-  |> List.dedup_and_sort ~compare
-  |> List.fold ~init:0 ~f:(+)
+let make_case_2 () =
+  let lst = ref [] in
+  for m1 = 100 to 999 do
+    for m2 = 10 to 99 do
+      let prod = m1 * m2 in
+      if prod < 10_000 then
+        lst := (m1 * (Int.pow 10 6) + m2 * (Int.pow 10 4) + prod, prod) :: !lst
+    done
+  done;
+  !lst
+
+let solve () =
+  let _, prod_lst = List.concat [make_case_1 (); make_case_2 ()]
+                    |> List.filter ~f:(fun (n, _) -> Euler.Math.is_pandigital_nz n)
+                    |> List.unzip
+  in
+  List.dedup_and_sort ~compare prod_lst |> List.fold ~init:0 ~f:(+)
 
 let exec () =
   Int.to_string (solve ())
