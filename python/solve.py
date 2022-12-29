458c9ebb73b3a0d2f67a66201a1ddfe6bd671957
kwj <kwj@vlax.net>
2022-12-29 08:54:55 -0800

    Add Python version check

--- python/solve.py
+++ python/solve.py
@@ -4,6 +4,22 @@ from functools import reduce
 import sys
 import euler
 
+# I use features which introduced in Python 3.10.
+min_major_version = 3
+min_minor_version = 10
+
+def version_check():
+    if sys.version_info.major < 3:
+        return False
+    if sys.version_info.major == min_major_version and sys.version_info.minor < min_minor_version:
+        return False
+
+    return True
+
+if version_check() == False:
+    raise RuntimeError('This solver requires Python {}.{} or later'.format(min_major_version, min_minor_version))
+
+
 args = sys.argv
 
 if len(args) > 1 and reduce(lambda x, y: x and str.isdecimal(y), args[1:], True):
