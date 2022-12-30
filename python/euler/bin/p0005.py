ab890b2525f7393eff06884de15fb607ca6424a5
kwj <kwj@vlax.net>
2022-12-29 16:14:02 -0800

    Add a solution to problem #5

---
+++ python/euler/bin/p0005.py
@@ -0,0 +1,15 @@
+
+# project euler: problem 5
+
+from time import perf_counter
+from math import lcm
+
+def compute(upper):
+    return lcm(*list(range(1, upper + 1)))
+
+def solve():
+    start = perf_counter()
+    result = compute(20)
+    elapsed_time = perf_counter() - start
+
+    return (result, "{:f}".format(elapsed_time))
