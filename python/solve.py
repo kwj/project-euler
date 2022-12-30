93faeb41070fc3189057d3fef5b187d937fa7637
kwj <kwj@vlax.net>
2022-12-29 23:38:20 -0800

    Update usage

--- python/solve.py
+++ python/solve.py
@@ -26,5 +26,5 @@ if len(args) > 1 and reduce(lambda x, y: x and str.isdecimal(y), args[1:], True)
     euler.main(args[1:])
 else:
     print('argument error')
-    print('  usage: {} num#1 num#2 ...'.format(args[0]))
+    print('  usage: {} num#1 [num#2 ...]'.format(args[0]))
     print('    num - problem number')
