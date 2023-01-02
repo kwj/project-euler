b1c0bfee545150a661af7f15ed04b8503285eea3
kwj <kwj@vlax.net>
2023-01-02 10:41:52 -0800

    Add a solution to problem #9

---
+++ python/euler/bin/p0009.py
@@ -0,0 +1,36 @@
+
+# project euler: problem 9
+
+# a = k(m^2 - n^2), b = k * 2mn, c = k(m^2 + n^2)  [m>n>0, gcd(m,n)=1, m+n is odd]
+#
+# abc = k^3 * (m^4 - n^4) * 2mn
+# a + b + c = k * 2m(m+n) = 1000
+#
+#  -> 'k' and 'm' are divisors to 500 (= 1000/2).
+#     'm+n' is a divisor to 500/m.
+#     m(m+n) <= 500 --> m <= isqrt(500), m+n <= 500/m
+
+from math import isqrt
+from time import perf_counter
+
+def compute(perim):
+    for m in range(2, isqrt(perim // 2) + 1):
+        if (perim // 2) % m != 0:
+            continue
+
+        x = m + 1 + (m % 2)     # x = m + n, x is odd number
+        while x < 2 * m and x <= (perim // 2) // m:
+            if (perim // 2) // m % x == 0:
+                k = (perim // 2) // m // x
+                n = x - m
+                return str(pow(k, 3) * (pow(m, 4) - pow(n, 4)) * 2 * m * n)
+            x += 2
+
+    return str(0)    # not found (NOT REACHED when perimeter = 1000)
+
+def solve():
+    start = perf_counter()
+    result = compute(1000)
+    elapsed_time = perf_counter() - start
+
+    return (result, "{:f}".format(elapsed_time))
