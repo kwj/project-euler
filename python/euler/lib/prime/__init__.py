736d5fb68d4279139fe57b46220c85f93a587918
kwj <kwj@vlax.net>
2023-01-11 09:22:58 -0800

    Remove eratosthenes.py

--- python/euler/lib/prime/__init__.py
+++ python/euler/lib/prime/__init__.py
@@ -3,4 +3,3 @@ from .isprime import is_prime, is_probably_prime
 from .sieve import sieve
 from .simple_sieve import get_primes, get_prime_tbl, tbl_to_primes
 from .generator import prime_generator
-from .eratosthenes import eratosthenes
