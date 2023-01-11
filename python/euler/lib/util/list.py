b89f087ab51f1cb3fe8bbc51146f30719ade617e
kwj <kwj@vlax.net>
2023-01-10 23:01:03 -0800

    Add assoc_group_dict()/assoc_group_lst()

--- python/euler/lib/util/list.py
+++ python/euler/lib/util/list.py
@@ -1,3 +1,15 @@
 
 def flatten(lst):
     return [item for sublist in lst for item in sublist]
+
+def assoc_group_dict(tpl_lst):
+    groups = dict()
+    for k, *v in tpl_lst:
+        if len(v) == 1:
+            v = v[0]
+        groups.setdefault(k, []).append(v)
+
+    return groups
+
+def assoc_group_lst(tpl_lst):
+    return list(assoc_group(tpl_lst).items())
