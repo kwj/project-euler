235c07c7e7dea9e444cda0b389e2c70b014ffe34
kwj <kwj@vlax.net>
2023-01-10 23:02:27 -0800

    Add a solution to problem #15

---
+++ python/tests/bin/test_p0015.py
@@ -0,0 +1,17 @@
+
+import unittest
+from euler.bin.p0015 import compute
+
+class TestSolution(unittest.TestCase):
+    def test_compute(self):
+        test_patterns = [
+            ((2,2), '6'),
+            ((20,20), '137846528820')
+        ]
+
+        for args, expected in test_patterns:
+            with self.subTest(f'grid: {args}'):
+                self.assertEqual(expected, compute(*args))
+
+if __name__ == '__main__':
+    unittest.main()
