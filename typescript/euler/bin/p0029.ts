595cd95c9a26ddf2ea7c12203a57e74e6d14c3f5
kwj <kwj@vlax.net>
2023-02-20 12:48:55 -0800

    Use another algorithm instead of brute force approach

--- typescript/euler/bin/p0029.ts
+++ typescript/euler/bin/p0029.ts
@@ -1,21 +1,27 @@
 
 // project euler: problem 29
 
+import { isqrt, lcm } from "../lib/math.ts";
 import { range } from "../lib/util.ts";
 
-function bruteForce(upper: number): number {
-  const n_set = new Set();
-  for (const a of range(2, upper + 1)) {
-    for (const b of range(2, upper + 1)) {
-      n_set.add(a ** b);
-    }
-  }
-
-  return Array.from(n_set).length;
+function log(x: number, y: number): number {
+  return Math.log(y) / Math.log(x);
 }
 
 export function compute(upper: number): string {
-  return String(bruteForce(upper));
+  const dup_tbl = Array.from(Array(upper + 1), _ => Array(upper + 1).fill(0));
+  for (const x of range(2, isqrt(upper) + 1)) {
+    for (const y of range(2, Math.floor(log(x, upper)) + 1)) {
+      for (const z of range(1, y)) {
+        const k = lcm(y, z) / y;
+        for (let i = Math.max(k, 2); i <= (upper * z / y); i += k) {
+          dup_tbl[x ** y][i] = 1;
+        }
+      }
+    }
+  }
+
+  return String((upper - 1) ** 2 - dup_tbl.flat().filter((n) => n === 1).length);
 }
 
 export function solve(): void {
