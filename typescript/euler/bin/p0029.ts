b2985c81b0bd2df91bb77155e9b52f6df83f81a0
kwj <kwj@vlax.net>
2023-02-22 15:53:51 -0800

    Improve algorithm

--- typescript/euler/bin/p0029.ts
+++ typescript/euler/bin/p0029.ts
@@ -1,27 +1,49 @@
 
 // project euler: problem 29
 
-import { isqrt, lcm } from "../lib/math.ts";
+import { getMaxExp, isqrt, lcm, sum } from "../lib/math.ts";
 import { range } from "../lib/util.ts";
 
-function log(x: number, y: number): number {
-  return Math.log(y) / Math.log(x);
+function make_dupctr_tbl(upper: number): number[] {
+  const max_exp: number = getMaxExp(upper, 2);
+  const dup_ctr: number[] = new Array(max_exp + 1);
+  const dups: number[] = new Array(upper + 1)
+
+  for (const x of range(2, max_exp + 1)) {
+    dups.fill(0);
+    for (const y of range(1, x)) {
+      const k = lcm(x, y) / x;
+      for (let i = Math.max(k, 2); i <= (upper * y / x); i += k) {
+        dups[i] = 1;
+      }
+    }
+  dup_ctr[x] = sum(dups);
+  }
+
+  return dup_ctr;
 }
 
 export function compute(upper: number): string {
-  const dup_tbl = Array.from(Array(upper + 1), _ => Array(upper + 1).fill(0));
-  for (const x of range(2, isqrt(upper) + 1)) {
-    for (const y of range(2, Math.floor(log(x, upper)) + 1)) {
-      for (const z of range(1, y)) {
-        const k = lcm(y, z) / y;
-        for (let i = Math.max(k, 2); i <= (upper * z / y); i += k) {
-          dup_tbl[x ** y][i] = 1;
-        }
+  const dup_ctr: number[] = make_dupctr_tbl(upper);
+  const base_limit: number = isqrt(upper);
+  const skip_flag: boolean[] = new Array(base_limit + 1);
+  skip_flag.fill(false);
+
+  let ans = (upper - 1) ** 2;
+  for (const b of range(2, base_limit + 1)) {
+    if (skip_flag[b] === true) {
+      continue;
+    }
+    for (const e of range(2, getMaxExp(upper, b) + 1)) {
+      ans -= dup_ctr[e];
+      const tmp = b ** e;
+      if (tmp <= base_limit) {
+        skip_flag[tmp] = true;
       }
     }
   }
 
-  return String((upper - 1) ** 2 - dup_tbl.flat().filter((n) => n === 1).length);
+  return String(ans);
 }
 
 export function solve(): void {
