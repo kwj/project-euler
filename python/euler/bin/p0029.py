f4a68c22888acd39e3bffdc6945928c33aecba69
kwj <kwj@vlax.net>
2023-02-22 13:51:54 -0800

    Improve algorithm

--- python/euler/bin/p0029.py
+++ python/euler/bin/p0029.py
@@ -2,19 +2,38 @@
 # project euler: problem 29
 
 from math import isqrt, lcm
-from euler.lib.util import flatten, get_max_exp
+from euler.lib.util import get_max_exp
 from time import perf_counter
 
-def compute(upper):
-    dup_tbl = [[0 for i in range(upper + 1)] for j in range(upper + 1)]
-    for x in range(2, isqrt(upper) + 1):
-        for y in range(2, get_max_exp(upper, base=x) + 1):
-            for z in range(1, y):
-                k = lcm(y, z) // y
-                length = len(dup_tbl[x ** y][max(k, 2):(upper * z // y + 1):k])
-                dup_tbl[x ** y][max(k, 2):(upper * z // y + 1):k] = [1] * length
+def make_dupctr_tbl(upper):
+    max_exp = get_max_exp(upper, base=2)
+    dup_ctr = [0] * (max_exp + 1)
 
-    return str((upper - 1) ** 2 - flatten(dup_tbl).count(1))
+    for x in range(2, max_exp + 1):
+        dups = [0] * (upper + 1)
+        for y in range(1, x):
+            k = lcm(x, y) // x
+            length = len(range(max(k, 2), (upper * y // x) + 1, k))
+            dups[max(k, 2):(upper * y // x) + 1:k] = [1] * length
+        dup_ctr[x] = sum(dups)
+
+    return dup_ctr
+
+def compute(upper):
+    dup_ctr = make_dupctr_tbl(upper)
+    exp_ctr = [0] * len(dup_ctr)
+    base_limit = isqrt(upper)
+    skip_flag = [False] * (base_limit + 1)
+
+    for b in range(2, base_limit + 1):
+        if skip_flag[b] == True:
+            continue
+        for e in range(2, get_max_exp(upper, base=b) + 1):
+            exp_ctr[e] += 1
+            if (tmp := b ** e) <= base_limit:
+                skip_flag[tmp] = True
+
+    return str((upper - 1) ** 2 - sum(map(lambda x: x[0] * x[1], zip(dup_ctr, exp_ctr))))
 
 def solve():
     start = perf_counter()
