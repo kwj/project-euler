b7998dff2c96c94cbdebe3b5bc95b7733377f583
kwj <kwj@vlax.net>
2023-02-25 00:25:35 -0800

    Use another algorithm instead of combination

--- python/euler/bin/p0053.py
+++ python/euler/bin/p0053.py
@@ -9,40 +9,23 @@ Pascal's triangle
   C(n,0) = C(n,n) = 1
 '''
 
-from math import factorial
 from time import perf_counter
 
-def comb(n, r):
-    return factorial(n) // factorial(r) // factorial(n - r)
-
-def find_r(n, r, boundary):
-    if (crnt := comb(n, r)) > boundary:
-        # Search to the left
-        while comb(n, r - 1) > boundary:
-            r -= 1
-        return r
-    else:
-        # Search to the right
-        while (right := comb(n, r + 1)) <= boundary:
-            if crnt > right:
-                return None
-            r += 1
-        return r + 1
-
 def compute(num, boundary):
-    # Search for the smallest 'r' where n = 100 and nCr > boundary.
-    r = 0
-    while comb(num, r) <= boundary:
-        r += 1
+    data = [1] * (num + 1)
+    ans = 0
 
-    # lst = [(n, r), ...] : 'r' is the smallest value when nCr is grater than boundary.
-    lst = []
-    for n in range(num, 0, -1):
-        if (r := find_r(n, r, boundary)) is None:
-            break
-        lst.append((n, r))
+    for start in range(1, num):
+        stop = (start + 2) // 2 - 1
+        for i in range(start, stop, -1):
+            data[i] = data[i] + data[i - 1]
+            if data[i] > boundary:
+                ans += i - (start - i)
+                break
+        if start % 2 == 0:
+            data[stop] = data[stop + 1]
 
-    return str(sum(map(lambda tpl: tpl[0] + 1 - 2 * tpl[1], lst)))
+    return str(ans)
 
 def solve():
     start = perf_counter()
