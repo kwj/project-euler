5e38874dcd330642fecece4b8611905c7403c89e
kwj <kwj@vlax.net>
2023-02-27 03:50:35 -0800

    Make search algorithm more simple

--- typescript/euler/bin/p0053.ts
+++ typescript/euler/bin/p0053.ts
@@ -1,52 +1,25 @@
 
 // project euler: problem 53
 
-import { factorial, sum } from "../lib/math.ts";
-import { range } from "../lib/util.ts";
-
-function comb(n: number, r: number) {
-  return Math.trunc(Math.trunc(factorial(n) / factorial(r)) / factorial(n - r));
-}
-
-function find_r(n: number, r: number, boundary: number): number | undefined {
-  const crnt = comb(n, r);
-  if (crnt > boundary) {
-    // search to the left
-    while (comb(n, r - 1) > boundary) {
-      r -= 1;
-    }
-    return r;
-  } else {
-    // search to the right
-    let right = comb(n, r + 1);
-    while (right <= boundary) {
-      if (crnt > right) {
-        return undefined;
-      }
-      r += 1;
-      right = comb(n, r + 1);
-    }
-    return r + 1;
-  }
-}
-
 export function compute(num: number, boundary: number): string {
-  let r = 0;
-  while (comb(num, r) <= boundary) {
-    r += 1;
-  }
+  let n = num, r = 1;
+  let c = 1, x = n;
+  let answer = 0;
 
-  const lst: [number, number][] = [];
-  for (const n of range(num, 0, -1)) {
-    const next_r = find_r(n, r, boundary);
-    if (next_r === undefined) {
-      break;
+  while (r < (n + 1) / 2) {
+    c = c * x / r;
+    if (c > boundary) {
+      answer += n - (r * 2) + 1;
+      c = c * r / n;
+      n -= 1;
+      x -= 1;
+    } else {
+      r += 1;
+      x -= 1;
     }
-    lst.push([n, next_r]);
-    r = next_r;
   }
 
-  return String(sum(lst.map((x) => x[0] + 1 - 2 * x[1])));
+  return String(answer)
 }
 
 export function solve(): void {
