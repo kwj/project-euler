8ab472c65731f9c86198c074aed639de954831d4
kwj <kwj@vlax.net>
2023-03-15 01:01:11 -0700

    Rename variable name

--- julia/Euler/src/bin/p0025.jl
+++ julia/Euler/src/bin/p0025.jl
@@ -7,8 +7,8 @@
 
 module Prob0025
 
-function solve_0025(digits = 1_000)
-    boundary = big(10) ^ (digits - 1)
+function solve_0025(n_digits = 1_000)
+    boundary = big(10) ^ (n_digits - 1)
     fₙ = big(1); fₙ₋₁ = big(1)
     idx = 2
     while fₙ < boundary
