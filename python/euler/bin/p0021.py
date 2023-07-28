d2109f6b25be404878b770ce219bfa818c469610
kwj <kwj@vlax.net>
2023-07-27 17:51:53 -0700

    Use get_sigma_tbl()

--- python/euler/bin/p0021.py
+++ python/euler/bin/p0021.py
@@ -1,34 +1,17 @@
 
 # project euler: problem 21
 
+from euler.lib.util import get_sigma_tbl
 from math import isqrt
 from time import perf_counter
 
-def d(n):
-    lst = []
-    for i in range(1, isqrt(n) + 1):
-        if n % i != 0:
-            continue
-        if i * i == n:
-            lst.append(i)
-        else:
-            lst.append(i)
-            lst.append(n//i)
-
-    return sum(lst) - n
-
 def compute(num):
-    num_lst = []
-    for i in range(2, num):
-        if i in num_lst:
-            continue
-        d1 = d(i)
-        d2 = d(d1)
-        if i == d2 and d1 != d2:
-            num_lst.append(d1)
-            num_lst.append(d2)
+    upper = num - 1
+    d_tbl = get_sigma_tbl(1, upper)
+    for x in range(upper + 1):
+        d_tbl[x] -= x
 
-    return str(sum(num_lst))
+    return str(sum(x + d_tbl[x] for x in range(2, upper + 1) if x > d_tbl[x] and d_tbl[d_tbl[x]] == x))
 
 def solve():
     start = perf_counter()
