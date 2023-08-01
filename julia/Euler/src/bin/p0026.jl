2b88a0cce2c04687c5fb4952b1c12643504fda29
kwj <kwj@vlax.net>
2023-08-01 11:36:36 -0700

    Improve performance when 'd' is large

--- julia/Euler/src/bin/p0026.jl
+++ julia/Euler/src/bin/p0026.jl
@@ -3,34 +3,50 @@
 
 module Prob0026
 
-function find_cycle_length(n, d)
-    rems = Dict{Int, Int}()
-    cnt = 0
-    while true
-        if n == 0
-            return 0
-        end
-        if haskey(rems, n) == true
-            return (cnt - rems[n])
-        end
+import Primes: factor
+import ..Util: divisors
 
-        rems[n] = cnt
-        n = (n * 10) % d
-        cnt += 1
+@inline function pp(n)
+    while n % 2 == 0
+        n ÷= 2
     end
+    while n % 5 == 0
+        n ÷= 5
+    end
+    n
+end
+
+function totient(n)
+    result = 1
+    for (b, e) in factor(Dict, n)
+        result *= (b - 1) * (b ^ (e - 1))
+    end
+    result
+end
+
+function find_repetend_length(d)
+    d = pp(d)
+    if d == 1
+        return 0
+    end
+    for k in divisors(totient(d))
+        if powermod(10, k, d) == 1
+            return k
+        end
+    end
+    @assert false "Not Reached"
 end
 
 function solve_0026(upper::Int = 1_000)
-    upper -= 1
-    (max_cycle, idx) = (0, 0)
+    (max_length, idx) = (0, 0)
 
-    for i = upper:-1:1
-        if i <= max_cycle
+    for i = (upper - 1):-1:(upper ÷ 2)
+        if i <= max_length
             break
         end
-        cycle_len = find_cycle_length(1, i)
-        if cycle_len > max_cycle
-            (max_cycle, idx) = (cycle_len, i)
+        repetend_length = find_repetend_length(i)
+        if repetend_length > max_length
+            (max_length, idx) = (repetend_length, pp(i))
         end
     end
     idx
