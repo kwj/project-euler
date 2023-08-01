0264bf15191b356c7aebf9e315757b5b540f85dd
kwj <kwj@vlax.net>
2023-08-01 12:54:23 -0700

    Improve performance when 'd' is large

--- python/euler/bin/p0026.py
+++ python/euler/bin/p0026.py
@@ -1,31 +1,42 @@
 
 # project euler: problem 26
 
+from functools import reduce
+from euler.lib.util import factorize, divisors
 from time import perf_counter
 
-def div_loop(a, b):
-    rems = dict()
-    cnt = 0
-    while True:
-        if a == 0:
-            return 0
-        if a in rems:
-            return (cnt - rems[a])
-        else:
-            rems[a] = cnt
-            a = (a * 10) % b
-            cnt += 1
+def pp(n):
+    while n % 2 == 0:
+        n //= 2
+    while n % 5 == 0:
+        n //= 5
+
+    return n
+
+def totient(n):
+    return reduce(lambda x, y: x * y, map(lambda x: (x[0] - 1) * (x[0] ** (x[1] - 1)), factorize(n)))
+
+def find_repetend_length(d):
+    if (d := pp(d)) == 1:
+        return 0
+
+    acc = 1
+    for k in divisors(totient(d)):
+        if pow(10, k, d) == 1:
+            return k
+
+    assert False, 'not reached'
 
 def compute(limit):
-    max_cycle = 0
+    max_length = 0
     num = 0
-    for i in range(limit - 1, 1, -1):
-        if i <= max_cycle:
+    for i in range(limit - 1, ((limit // 2) - 1), -1):
+        if i <= max_length:
             break
-        cycle_len = div_loop(1, i)
-        if cycle_len > max_cycle:
-            num = i
-            max_cycle = cycle_len
+        repetend_length = find_repetend_length(i)
+        if repetend_length > max_length:
+            num = pp(i)
+            max_length = repetend_length
 
     return str(num)
 
