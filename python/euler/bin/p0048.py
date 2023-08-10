e313df39094e3a6e46e2f972ee667ce0557d8dac
kwj <kwj@vlax.net>
2023-08-09 17:24:50 -0700

    Use the 'mod' argument of pow()

--- python/euler/bin/p0048.py
+++ python/euler/bin/p0048.py
@@ -6,7 +6,7 @@ from time import perf_counter
 def compute(exp):
     modulus = 10 ** 10
 
-    return format(sum(pow(x, x) % modulus for x in range(1, exp + 1) if x % 10 != 0) % modulus, '010')
+    return format(sum(pow(x, x, modulus) for x in range(1, exp + 1) if x % 10 != 0) % modulus, '010')
 
 def solve():
     start = perf_counter()
