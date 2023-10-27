26de6aa6326796d0f86ba2eebd7cb3931d5d6ccf
kwj <kwj@vlax.net>
2023-10-27 15:13:33 -0700

    Use recursive function

--- ocaml/src/p0017.ml
+++ ocaml/src/p0017.ml
@@ -2,61 +2,61 @@
 
 open Core
 
-let words =
-  [ (1, String.length "one")
-  ; (2, String.length "two")
-  ; (3, String.length "three")
-  ; (4, String.length "four")
-  ; (5, String.length "five")
-  ; (6, String.length "six")
-  ; (7, String.length "seven")
-  ; (8, String.length "eight")
-  ; (9, String.length "nine")
-  ; (10, String.length "ten")
-  ; (11, String.length "eleven")
-  ; (12, String.length "twelve")
-  ; (13, String.length "thirteen")
-  ; (14, String.length "fourteen")
-  ; (15, String.length "fifteen")
-  ; (16, String.length "sixteen")
-  ; (17, String.length "seventeen")
-  ; (18, String.length "eighteen")
-  ; (19, String.length "nineteen")
-  ; (20, String.length "twenty")
-  ; (30, String.length "thirty")
-  ; (40, String.length "forty")
-  ; (50, String.length "fifty")
-  ; (60, String.length "sixty")
-  ; (70, String.length "seventy")
-  ; (80, String.length "eighty")
-  ; (90, String.length "ninety")
-  ; (0, 0) (* special data *)
+let under_20 =
+  [ 0
+  ; String.length "one"
+  ; String.length "two"
+  ; String.length "three"
+  ; String.length "four"
+  ; String.length "five"
+  ; String.length "six"
+  ; String.length "seven"
+  ; String.length "eight"
+  ; String.length "nine"
+  ; String.length "ten"
+  ; String.length "eleven"
+  ; String.length "twelve"
+  ; String.length "thirteen"
+  ; String.length "fourteen"
+  ; String.length "fifteen"
+  ; String.length "sixteen"
+  ; String.length "seventeen"
+  ; String.length "eighteen"
+  ; String.length "nineteen"
   ]
 ;;
 
-let count_letters num =
-  match num with
-  | 1000 -> 11 (* one thousand *)
-  | n when n < 20 -> List.Assoc.find_exn words n ~equal
+let mults_10 =
+  [ 0
+  ; 0
+  ; String.length "twenty"
+  ; String.length "thirty"
+  ; String.length "forty"
+  ; String.length "fifty"
+  ; String.length "sixty"
+  ; String.length "seventy"
+  ; String.length "eighty"
+  ; String.length "ninety"
+  ]
+;;
+
+let nchars_1000 = String.length "thousand"
+let nchars_100 = String.length "hundred"
+let nchars_and = String.length "and"
+
+let rec count_letters = function
+  | 1000 -> List.nth_exn under_20 1 + nchars_1000
+  | n when n < 20 -> List.nth_exn under_20 n
   | n when n < 100 ->
-    List.Assoc.find_exn words (n - (n mod 10)) ~equal
-    + List.Assoc.find_exn words (n mod 10) ~equal
-  | n when n mod 100 = 0 ->
-    (* X00 hundred *)
-    List.Assoc.find_exn words (n / 100) ~equal + 7
-  | n when n mod 100 < 20 ->
-    (* X hundred and YZ (YZ < 20) *)
-    List.Assoc.find_exn words (n / 100) ~equal
-    + 7
-    + 3
-    + List.Assoc.find_exn words (n mod 100) ~equal
+    let q = n / 10
+    and r = n mod 10 in
+    List.nth_exn mults_10 q + List.nth_exn under_20 r
   | n ->
-    (* X hundred and YZ (20 <= YZ <= 99) *)
-    List.Assoc.find_exn words (n / 100) ~equal
-    + 7
-    + 3
-    + List.Assoc.find_exn words ((n mod 100) - (n mod 10)) ~equal
-    + List.Assoc.find_exn words (n mod 10) ~equal
+    let q = n / 100
+    and r = n mod 100 in
+    List.nth_exn under_20 q
+    + nchars_100
+    + if r = 0 then 0 else nchars_and + count_letters r
 ;;
 
 let compute limit =
