37170ad8577ab4da3d1f727495ec91d7022ebe99
kwj <kwj@vlax.net>
2023-11-01 13:09:53 -0700

    Reformat

--- python/tests/bin/test_p0050.py
+++ python/tests/bin/test_p0050.py
@@ -5,7 +5,12 @@ from euler.bin.p0050 import compute
 
 class TestSolution(unittest.TestCase):
     def test_compute(self):
-        test_patterns = [((100,), '41'), ((1_000,), '953'), ((10_000,), '9521'), ((1_000_000,), '997651')]
+        test_patterns = [
+            ((100,), '41'),
+            ((1_000,), '953'),
+            ((10_000,), '9521'),
+            ((1_000_000,), '997651'),
+        ]
 
         for args, expected in test_patterns:
             with self.subTest(f'upper: {args}'):
