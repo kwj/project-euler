0086f46bee2189cdf8f98389be77547d71424a20
kwj <kwj@vlax.net>
2023-11-30 17:49:50 -0800

    Use vector instead of the cycle function

--- clojure/src/project_euler/sol/p0059.clj
+++ clojure/src/project_euler/sol/p0059.clj
@@ -11,27 +11,30 @@
 (defn- eval-char
   [c]
   (cond
-    (= c 0x20) 3 ; space
-    (<= 0x41 c 0x5A) 5 ; uppercase letters
     (<= 0x61 c 0x7A) 2 ; lowercase letters
+    (<= 0x41 c 0x5A) 5 ; uppercase letters
     (<= 0x21 c 0x7E) 1 ; printable characters (exclude letters)
+    (= c 0x20) 3 ; space
     :else 0))
 
 (defn- eval-score
-  [cipher-data key]
-  (let [plain-text (map bit-xor cipher-data (cycle key))
-        score (->> (map #(eval-char %) plain-text)
-                   (apply +))]
-    [score (apply + plain-text) key]))
+  [data key]
+  [(->> (map-indexed (fn [idx ch] (eval-char (bit-xor ch (nth key (mod idx 3)))))
+                     data)
+        (apply +))
+   key])
 
 (defn solve
   ([]
    (solve (util/read-data "0059_cipher.txt")))
   ([data]
-   (let [encrypted-data (parse-data data)]
-     (->> (util/cartesian-product (range (int \a) (inc (int \z)))
-                                  (range (int \a) (inc (int \z)))
-                                  (range (int \a) (inc (int \z))))
-          (map #(eval-score encrypted-data %))
-          (apply max-key first)
-          (second)))))
+   (let [encrypted-data (parse-data data)
+         key (->> (util/cartesian-product (range (int \a) (inc (int \z)))
+                                          (range (int \a) (inc (int \z)))
+                                          (range (int \a) (inc (int \z))))
+                  (map #(eval-score encrypted-data (vec %)))
+                  (apply max-key first)
+                  (second))]
+     (->> (map-indexed (fn [idx ch] (bit-xor ch (nth key (mod idx 3))))
+                       encrypted-data)
+          (apply +)))))
