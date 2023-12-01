789a6fd07fba56070b931309c1cbacb9e2771421
kwj <kwj@vlax.net>
2023-11-30 17:49:36 -0800

    Check combinations of digits instead of all numbers

--- clojure/src/project_euler/sol/p0074.clj
+++ clojure/src/project_euler/sol/p0074.clj
@@ -3,38 +3,59 @@
 ;;;   It therefore doesn't work well for all chains.
 
 (ns project-euler.sol.p0074
-  (:require [project-euler.lib.util :as util]))
+  (:require
+   [project-euler.lib.math :as math]
+   [project-euler.lib.util :as util]))
+
+(defn n-perms [lst]
+  (long (reduce (fn [acc x] (quot acc (math/factorial x)))
+                (math/factorial (apply + lst))
+                lst)))
+
+(defn- num-of-valid-numbers
+  [digit-lst]
+  (let [n-digits (count digit-lst)
+        digit-map (frequencies digit-lst)
+        n-ones (get digit-map 1 0)]
+    (letfn [(aux [n-zero n-one lst]
+              (if (= n-digits n-zero)
+                0
+                (* (math/binomial (dec n-digits) n-zero)
+                   (n-perms (conj lst n-one)))))]
+      (apply + (map #(aux (- n-ones %) % (vals (dissoc digit-map 1)))
+                    (range 0 (inc n-ones)))))))
 
 (defn- next-number
   [n]
   (let [fact-tbl [1 1 2 6 24 120 720 5040 40320 362880]]
     (if (zero? n)
-      (get fact-tbl 0)
-      (loop [n n acc 0]
+      (nth fact-tbl 0)
+      (loop [n n
+             acc 0]
         (if (zero? n)
           acc
-          (recur (quot n 10) (long (+ acc (get fact-tbl (mod n 10))))))))))
+          (recur (quot n 10) (long (+ acc (nth fact-tbl (mod n 10))))))))))
 
 (defn- find-chain
-  [d]
-  (let [n (util/undigits d)]
-    (loop [next-n (next-number n), l '(), s #{}]
+  [digit-lst]
+  (let [init-num (util/undigits digit-lst)]
+    (loop [next-n (next-number init-num)
+           l '()
+           s #{}]
       (if (contains? s next-n)
-        (if (= (sort (util/digits (first l))) (sort d))
-          (if (= (first l) n)
+        (if (= (sort (util/digits (first l))) digit-lst)
+          (if (= (first l) init-num)
             l
-            (conj l n))
-          (conj (reverse l) n))
+            (conj l init-num))
+          (conj (reverse l) init-num))
         (recur (next-number next-n) (conj l next-n) (conj s next-n))))))
 
 (defn solve
   ([]
-   (solve 1000000))
-  ([upper]
-   (->> (range 1 upper)
-        (map #(sort (util/digits %)))
-        (frequencies)
-        (map #(vector (count (find-chain (first %))) (second %)))
-        (filter #(= (first %) 60))
-        (map #(second %))
+   (solve 6))
+  ([n-digit]
+   (->> (map #(util/combination-with-repetition % (range 1 10)) (range 1 (inc n-digit)))
+        (apply concat)
+        (filter #(= (count (find-chain %)) 60))
+        (map num-of-valid-numbers)
         (apply +))))
