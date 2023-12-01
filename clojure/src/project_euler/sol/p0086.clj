b5866ba03c33fc1ef9fa3d1f4e88d289939a8722
kwj <kwj@vlax.net>
2023-11-30 17:44:57 -0800

    Split the solve function for readability

--- clojure/src/project_euler/sol/p0086.clj
+++ clojure/src/project_euler/sol/p0086.clj
@@ -1,21 +1,27 @@
 (ns project-euler.sol.p0086
-  (:require [project-euler.lib.math :as math]))
+  (:require
+   [clojure.math]))
+
+(defn- aux
+  ^long [^long c]
+  (loop [ab (* c 2)
+         acc 0]
+    (if (> ab 1)
+      (let [tmp (+ (* c c) (* ab ab))
+            tmp-isqrt (long (clojure.math/floor (clojure.math/sqrt tmp)))]
+        (if (= (* tmp-isqrt tmp-isqrt) tmp)
+          (cond
+            (<= ab c) (recur (dec ab) (+ acc (quot ab 2)))
+            :else (recur (dec ab) (+ acc (- (quot ab 2) (- ab 1 c)))))
+          (recur (dec ab) acc)))
+      acc)))
 
 (defn solve
   ([]
    (solve 1000000))
   ([thr]
    (loop [c 3
-          acc 0]
-     (if (> acc thr)
+          cnt 0]
+     (if (> cnt thr)
        (dec c)
-       (recur (inc c)
-              (long (+ acc
-                       (loop [ab (* c 2)
-                              acc 0]
-                         (if (< ab 2)
-                           acc
-                           (cond
-                             (not (math/square? (+ (* c c) (* ab ab)))) (recur (dec ab) acc)
-                             (<= ab c) (recur (dec ab) (+ acc (quot ab 2)))
-                             :else (recur (dec ab) (- (+ acc (quot ab 2)) (- ab 1 c)))))))))))))
+       (recur (inc c) (+ cnt (aux c)))))))
