d7788d6f0023d58e09defbce2004a008a038269b
kwj <kwj@vlax.net>
2024-06-16 23:49:50 -0700

    Make the check_square function more simple

--- rust/src/bin/p0090.rs
+++ rust/src/bin/p0090.rs
@@ -13,29 +13,29 @@ fn compute() -> usize {
         .into_iter()
         .combinations(6)
         .combinations_with_replacement(2)
-        .filter(|dice| check_square(dice))
+        .filter(|two_dice| check_square(two_dice))
         .count()
 }
 
-fn check_square(two_dice: &[Vec<i32>]) -> bool {
-    assert!(two_dice.len() == 2);
+fn check_square(dice: &[Vec<i32>]) -> bool {
+    assert!(dice.len() == 2);
 
-    // Squared list with 9 replaced by 6
-    const SQUARES: [[[i32; 2]; 2]; 8] = [
-        [[1, 0], [0, 1]],
-        [[4, 0], [0, 4]],
-        [[6, 0], [0, 6]],
-        [[6, 1], [1, 6]],
-        [[5, 2], [2, 5]],
-        [[6, 3], [3, 6]],
-        [[6, 4], [4, 6]],
-        // [[4, 6], [6, 4]],  8^2 = 64, 7^2 = 49 -> 46.  So this check can be omitted.
-        [[1, 8], [8, 1]],
+    // Squares list which is replaced 9's with 6
+    const SQUARES: [[i32; 2]; 8] = [
+        [0, 1],
+        [0, 4],
+        [0, 6],
+        [1, 6],
+        [2, 5],
+        [3, 6],
+        [4, 6],
+        // [6, 4],  8^2 = 64, 7^2 = 49 -> 46.  So, this term is omitted.
+        [8, 1],
     ];
 
-    SQUARES.iter().all(|arr| {
-        arr.iter()
-            .any(|sq| two_dice[0].contains(&sq[0]) && two_dice[1].contains(&sq[1]))
+    SQUARES.iter().all(|die| {
+        dice[0].contains(&die[0]) && dice[1].contains(&die[1])
+            || dice[0].contains(&die[1]) && dice[1].contains(&die[0])
     })
 }
 
