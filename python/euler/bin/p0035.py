1140b8966d5a0a7846edd51582fd754aa21b404f
kwj <kwj@vlax.net>
2024-06-16 20:41:13 -0700

    Use numerical calculations instead of string operations

--- python/euler/bin/p0035.py
+++ python/euler/bin/p0035.py
@@ -1,16 +1,21 @@
 # project euler: problem 35
 
 from euler.lib.prime import is_prime, primes
+from euler.lib.util import num_of_digits
 
 
 def compute(limit: int) -> str:
-    def check_rot_nums(n: int) -> bool:
-        s = str(n) * 2
-        m = len(s) // 2
+    def is_circular_prime(n: int) -> bool:
+        k = num_of_digits(n) - 1
+        d = pow(10, k)
+        for _ in range(k):
+            n = (n % 10) * d + n // 10
+            if is_prime(n) is False:
+                return False
 
-        return all(is_prime(elm) for elm in (int(s[pos : pos + m]) for pos in range(m)))
+        return True
 
-    return str(len([n for n in primes(limit) if check_rot_nums(n) is True]))
+    return str(len([n for n in primes(limit) if is_circular_prime(n) is True]))
 
 
 def solve() -> str:
