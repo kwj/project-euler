cdd88a9b8aaae612965562a8c3ddf817cc4a9216
kwj <kwj@vlax.net>
2024-07-10 17:53:48 -0700

    Add a test case

--- typescript/euler/test/p0073_test.ts
+++ typescript/euler/test/p0073_test.ts
@@ -2,6 +2,12 @@ import { assertEquals } from "@std/assert";
 import { compute } from "../bin/p0073.ts";
 
 Deno.test("normal case 1", () => {
+  const actual = compute(8);
+  const expected = "3";
+  assertEquals(actual, expected);
+});
+
+Deno.test("normal case 2", () => {
   const actual = compute(12_000);
   const expected = "7295372";
   assertEquals(actual, expected);
