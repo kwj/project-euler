c1d756d76cdd7a90092e60b7f25879e400f9f4c4
kwj <kwj@vlax.net>
2024-07-27 11:02:29 -0700

    Make makeAmicableChains() more simple

--- haskell/src/Sol/P0095.hs
+++ haskell/src/Sol/P0095.hs
@@ -4,7 +4,7 @@ module Sol.P0095 (compute, solve) where
 
 import Data.Array.Unboxed ((!))
 import Data.Function (on)
-import Data.List (maximumBy)
+import Data.List (elemIndex, maximumBy)
 import Data.Maybe (fromJust)
 
 import Mylib.Factor (aliquotSumTbl)
@@ -13,33 +13,18 @@ import Mylib.Factor (aliquotSumTbl)
 --       but also perfect numbers and amicable pairs.
 makeAmicableChains :: Int -> [[Int]]
 makeAmicableChains limit =
-    aux [2 .. limit] []
+    map fromJust . filter (/= Nothing) $ aux [] <$> [2 .. limit]
   where
     nextPosTbl = aliquotSumTbl limit
 
-    aux :: [Int] -> [Maybe [Int]] -> [[Int]]
-    aux [] chains = map fromJust $ filter (/= Nothing) chains
-    aux (x : xs) chains =
-        aux xs ((go x []) : chains)
-
-    go :: Int -> [Int] -> Maybe [Int]
-    go n chain
+    aux :: [Int] -> Int -> Maybe [Int]
+    aux chain n
         | n <= 1 || n > limit =
             Nothing
-        | elem n chain == True =
-            let (l1, _) = splitLst chain n []
-             in Just l1
+        | Just idx <- elemIndex n chain =
+            Just $ take (idx + 1) chain
         | otherwise =
-            go (nextPosTbl ! n) (n : chain)
-
-    splitLst :: [Int] -> Int -> [Int] -> ([Int], [Int])
-    splitLst [] _ left = (reverse left, [])
-    splitLst (x : xs) n left =
-        if x == n
-            then
-                (reverse (x : left), xs)
-            else
-                splitLst xs n (x : left)
+            aux (n : chain) (nextPosTbl ! n)
 
 compute :: Int -> String
 compute limit =
