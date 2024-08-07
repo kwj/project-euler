aa2d40457ceac182240bb094d03e5f3761c8295c
kwj <kwj@vlax.net>
2024-08-07 11:24:44 -0700

    Make code simpler

--- haskell/src/Sol/P0061.hs
+++ haskell/src/Sol/P0061.hs
@@ -4,22 +4,6 @@ import Data.List (delete, nub)
 
 import Mylib.Util (headExn, lastExn)
 
--- P(s,n)
--- A polygonal number is equal to an arithmetic series.
-polygonalNumbers :: Int -> [Int]
-polygonalNumbers s =
-    filter ((>= 10) . (`mod` 100))
-        . takeWhile (< 10000)
-        . dropWhile (< 1000)
-        $ scanl1 (+) [1, k ..]
-  where
-    k = s - 1
-
--- Triangle/Square/.../Heptagonal numbers
-polygonals_P3_to_P7 :: [[Int]]
-polygonals_P3_to_P7 =
-    polygonalNumbers <$> [3 .. 7]
-
 searchRest :: [Int] -> [[Int]] -> [[Int]]
 searchRest ns [] =
     [[] | headExn ns `mod` 100 == lastExn ns `div` 100] -- cyclic?
@@ -27,7 +11,7 @@ searchRest ns pss =
     [ p : rest
     | ps <- pss -- select a group of polygonals
     , p <- ps -- select a polygonal number from the group
-    , mod (headExn ns) 100 == p `div` 100 -- can it be connected?
+    , headExn ns `mod` 100 == p `div` 100 -- can it be connected?
     , rest <- searchRest (p : ns) (delete ps pss) -- If ok, search for next connectable polygonal number.
     ]
 
@@ -35,7 +19,7 @@ compute :: String
 compute =
     -- According to the problem statement, only one cycle exists.
     if length cands /= 1
-        then error "no answer exists"
+        then error "fatal error (only one answer must exist)"
         else show . sum $ headExn cands
   where
     cands =
@@ -44,8 +28,15 @@ compute =
                 nub
                 [ x : rest
                 | x <- polygonalNumbers 8 -- start searching from octagonal numbers
-                , rest <- searchRest [x] polygonals_P3_to_P7
+                , rest <- searchRest [x] (polygonalNumbers <$> [3 .. 7])
                 ]
+
+    polygonalNumbers :: Int -> [Int]
+    polygonalNumbers s =
+        filter ((>= 10) . (`mod` 100))
+            . takeWhile (< 10000)
+            . dropWhile (< 1000)
+            $ scanl1 (+) [1, (s - 1) ..]
 
 solve :: String
 solve = compute
