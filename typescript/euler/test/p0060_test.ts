abb08b5ae9138dca0132699aa639cb60815b3f03
kwj <kwj@vlax.net>
2024-08-15 07:44:54 -0700

    Improve performance of finding cliques

--- typescript/euler/test/p0060_test.ts
+++ typescript/euler/test/p0060_test.ts
@@ -2,7 +2,13 @@ import { assertEquals } from "@std/assert";
 import { compute } from "../bin/p0060.ts";
 
 Deno.test("normal case 1", () => {
-  const actual = compute();
+  const actual = compute(4);
+  const expected = "792";
+  assertEquals(actual, expected);
+});
+
+Deno.test("normal case 2", () => {
+  const actual = compute(5);
   const expected = "26033";
   assertEquals(actual, expected);
 });
