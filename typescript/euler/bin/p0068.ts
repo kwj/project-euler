a0bb583411e46bd07d1d05202366e1a6a6cfca62
kwj <kwj@vlax.net>
2024-08-16 19:22:08 -0700

    Improve performance by using another algorithm

--- typescript/euler/bin/p0068.ts
+++ typescript/euler/bin/p0068.ts
@@ -1,197 +1,109 @@
 // project euler: problem 68
 
 /*
-  This problem can be solved with pen and paper.
-  No computer is needed.
-
-  1) maximam 16-digit string
-    number '10' must be at outer position.
-
-  2) If the condition is met when the numbers '6' to '10' are at
-     outer positions, one of them is the answer.
-
-  --> Assume that numbers '6' to '10' are at the outer positions.
-
-  3) the number '1' to '5' are on the pentagon.
-
-  4) the sum of all lines is (1+2+3+4+5)*2 + (6+7+8+9+10) = 70.
-
-  5) the sum of numbers on each lines are all 14.
-
-  6) number '1' and '3' must be on the line which contains '10'.
-
-    case #1       case #2
-        10            10
-          \             \
-           3  8,9        1  6,7,8,9
-            \ /           \ /
-             1             3
-
-  7) case #1-1:
-          10
-            \
-             3   8
-              \ /
-               1
-              /
-             5
-
-       <a>                 <b>
-          10              10
-            \               \
-             3   8           3   8
-           /  \ /          /  \ /
-          4    1   [NG]   2    1    [NG]
-         /\   /           \   /
-       [7] 2-5--7          4-5-[5]
-            \
-             8
-
-     case #1-2:
-          10
-            \
-             3   9
-              \ /
-               1
-              /
-             4
-
-       <a>                 <b>
-          10                10
-            \                 \
-             3   9             3   9
-              \ /            /  \ /
-               1   [NG]     5    1   [OK] 6,5,3;10,3,1;9,1,4;8,4,2;7,2,5
-              /            /\   /
-           5-4-[5]        6  2-4--8
-                              \
-                               7
-
-
-
-        <e1>
-          a0
-            \   <e2>
-            a1  a3
-           /  \ /
-         a8   a2
-         /\   /
-       a9 a6-a4-a5 <e3>
-     <e5>   \
-             a7
-             <e4>
-
-  8) case #2-1:
-          10
-            \
-             1   9
-              \ /
-               3
-              /
-             2
-       <a>                 <b>
-          10                   10
-            \                    \
-             1   9                1   9
-           /  \ /               /  \ /
-          5    3   [NG]        4    3  [NG]
-          \   /                \   /
-           4-2                  5-2
-            \                    \
-            [5]                  [5]
-
-     case #2-2:
-          10
-            \
-             1   8
-              \ /
-               3   [NG]
-              /
-            [3]
-
-     case #2-3:
-          10
-            \
-             1   7
-              \ /
-               3
-              /
-             4
-
-       <a>                 <b>
-          10                   10
-            \                    \
-             1   7                1   7
-           /  \ /               /  \ /
-          5    3   [NG]        2    3  [NG]
-          \   /                \   /
-           2-4                  5-4
-            \                    \
-            [7]                  [7]
-
-     case #2-4:
-          10
-            \
-             1   6
-              \ /
-               3
-              /
-             5
-
-       <a>                 <b>
-          10                   10
-            \                    \
-             1   6                1   6
-           /  \ /               /  \ /
-          2    3   [NG]        4    3   [OK] 6,3,5;7,5,2;8,2,4;9,4,1;10,1;3
-          \   /               /\   /
-           4-5-[5]           9  2-5-7
-                                 \
-                                  8
-
-  9) There are two patterns which satisfy the condition.
-     The answer is case #1-2 <b>.
-      case #1-2 <b>
-        6,5,3;10,3,1;9,1,4;8,4,2;7,2,5
-      case #2-4 <b>
-        6,3,5;7,5,2;8,2,4;9,4,1;10,1;3
-
-  ----
-  I'll use brute force with the following conditions.
-
-  list [a0, a1, a2, a3, a4, a5, a6, a7, a8, a9]
-   - one of {a3, a5, a7, a9} is 10
-   - a0 < {a3, a5, a7, a9}
-   - each lines (e1..e5) have same weight
+  Note: This problem can be solved by pen and paper.
 */
 
-import { permutations } from "combinatorics/mod.ts";
-import { sum } from "../lib/math.ts";
 import { range } from "../lib/util.ts";
 
-export const compute = (): string => {
-  const result: string[] = [];
-  for (
-    const [a0, a1, a2, a3, a4, a5, a6, a7, a8, a9] of permutations(
-      range(1, 10 + 1),
-    )
-  ) {
-    if (a0 > a3 || a0 > a5 || a0 > a7 || a0 > a9) {
-      continue;
-    }
-    if (![a3, a5, a7, a9].includes(10)) {
-      continue;
-    }
-
-    // deno-fmt-ignore
-    const ring = [[a0, a1, a2], [a3, a2, a4], [a5, a4, a6], [a7, a6, a8], [a9, a8, a1]];
-    const edgeWeights = ring.map((x) => sum(x));
-    if (edgeWeights.every((x) => x === edgeWeights[0])) {
-      result.push(ring.map((x) => x.map((y) => String(y)).join("")).join(""));
-    }
+const allRings = (nGon: number): string[] => {
+  // weight: sum of each node on line
+  //   minimum weight: (n_gon * 2) + 1 + 2 = n_gon * 2 + 3
+  //   maximum weight: 1 + (n_gon * 2 - 1) + (n_gon * 2) = n_gon * 4
+  let result: string[] = [];
+  for (const weight of range(nGon * 2 + 3, nGon * 4 + 1)) {
+    result = result.concat(findRings(nGon, weight));
   }
 
-  return result.reverse()[0];
+  return result;
 };
 
-export const solve = (): string => compute();
+const deleteElmnt = <T>(elm: T, arr: T[]): T[] => arr.filter((x) => x !== elm);
+
+const findRings = (nGon: number, weight: number): string[] => {
+  const numbers = range(1, nGon * 2 + 1);
+  const rings: string[] = []; // rings found
+
+  const makeStr = (ring: number[]): string => {
+    let result = "";
+    for (let i = 0; i < nGon * 2; i += 2) {
+      result = `${result}${ring[i + 1]}${ring[i]}${ring[i + 2]}`;
+    }
+
+    return result;
+  };
+
+  // ring :: number[]
+  //   +-+-+--     ---+-+-+   X: first selected inner node -- ring[0]
+  //   |X|Y|   ...    | |Z|   Y: first selected outer node -- ring[1]
+  //   +-+-+--     ---+-+-+   Z: last selected inner node -- ring.at(-1)
+  //    0
+  //
+  //     [Y]
+  //       \
+  //        [X]   *
+  //       /   \ /
+  //     ??     *
+  //    / \    /
+  //  ??  [Z]-*-- *
+  //        \
+  //         ??
+  const nextStates = (
+    [ring, rest]: [number[], number[]],
+  ): [number[], number[]][] => {
+    const states: [number[], number[]][] = [];
+
+    if (rest.length === 1) {
+      const n = rest[0];
+      if (n > ring[1] && n + ring[0] + ring.at(-1)! === weight) {
+        // a magic n-gon ring found
+        rings.push(makeStr([...ring, n, ring[0]]));
+      }
+    } else {
+      for (const outer of rest) {
+        if (
+          ring.length === 1 && outer > nGon + 1 ||
+          ring.length > 1 && outer < ring[1]
+        ) {
+          continue;
+        }
+        const inner = weight - outer - ring.at(-1)!;
+        if (outer === inner) {
+          continue;
+        }
+        if (!rest.includes(inner)) {
+          continue;
+        }
+        states.push([
+          [...ring, outer, inner],
+          deleteElmnt(inner, deleteElmnt(outer, rest)),
+        ]);
+      }
+    }
+
+    return states;
+  };
+
+  let queue: [number[], number[]][] = [];
+  for (const x of numbers) {
+    queue.push([[x], deleteElmnt(x, numbers)]);
+  }
+
+  while (queue.length > 0) {
+    const state = queue.pop()!;
+    queue = queue.concat(nextStates(state));
+  }
+
+  return rings;
+};
+
+export const compute = (nGon: number): string => {
+  let result = allRings(nGon).toSorted();
+  if (nGon === 5) {
+    result = result.filter((x) => x.length === 16);
+  }
+
+  return result.at(-1)!;
+};
+
+export const solve = (): string => compute(5);
--- typescript/euler/test/p0068_test.ts
+++ typescript/euler/test/p0068_test.ts
@@ -2,7 +2,13 @@ import { assertEquals } from "@std/assert";
 import { compute } from "../bin/p0068.ts";
 
 Deno.test("normal case 1", () => {
-  const actual = compute();
+  const actual = compute(3);
+  const expected = "432621513";
+  assertEquals(actual, expected);
+});
+
+Deno.test("normal case 2", () => {
+  const actual = compute(5);
   const expected = "6531031914842725";
   assertEquals(actual, expected);
 });
