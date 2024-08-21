6d0b1922985f9303589e2117ed969df8b7682b02
kwj <kwj@vlax.net>
2024-08-21 07:02:33 -0700

    Make code simpler

--- rust/src/bin/p0019.rs
+++ rust/src/bin/p0019.rs
@@ -9,23 +9,27 @@ fn solve() -> String {
 fn compute() -> i64 {
     let common_year: Vec<i64> = vec![31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     let leap_year: Vec<i64> = vec![31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    // number of days in each month (from 'Jan 1901' to 'Dec 2000')
     let mut days = repeat_vec(&concat_vec(&repeat_vec(&common_year, 3), &leap_year), 25);
-    days.remove(days.len() - 1);
+    // insert the number of days in the year 1900 at the beginning
+    days.insert(0, 365);
+    // remove 'Dec 2000'
+    days.pop();
 
-    let mut cumsum_days: Vec<i64> = vec![1 + 365];
-    for d in days {
-        cumsum_days.push(cumsum_days.last().unwrap() + d);
-    }
-
-    cumsum_days.into_iter().filter(|&x| x % 7 == 0).count() as i64
+    // the `initial_state` parameter of Iterator::scan() is the day of the week on January 1, 1900
+    // (0: Sunday, 1: Monday, ..., 6: Saturday)
+    days.iter()
+        .scan(1, |state, &x| {
+            *state = (*state + x) % 7;
+            Some(*state)
+        })
+        .filter(|&x| x == 0)
+        .count() as i64
 }
 
-fn repeat_vec(v: &[i64], count: i64) -> Vec<i64> {
-    let mut result: Vec<i64> = Vec::new();
-    for _ in 0..count {
-        result.extend(v);
-    }
-    result
+fn repeat_vec(v: &[i64], count: usize) -> Vec<i64> {
+    v.iter().copied().cycle().take(v.len() * count).collect()
 }
 
 fn concat_vec(v1: &[i64], v2: &[i64]) -> Vec<i64> {
