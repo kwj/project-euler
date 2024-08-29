986bb636a758046cb2ca0a516a7ae22af6b3aba5
kwj <kwj@vlax.net>
2024-08-28 19:00:10 -0700

    Add a comment

--- julia/Euler/src/bin/p0001.jl
+++ julia/Euler/src/bin/p0001.jl
@@ -4,6 +4,7 @@
 module Prob0001
 
 function solve_0001(limit::Int = 1000)
+    # Although this code is inefficient, I wanted to try to use set-like operation.
     sum(union(3:3:(limit - 1), 5:5:(limit - 1)))
 end
 
