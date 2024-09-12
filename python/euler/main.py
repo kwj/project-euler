fc1ae4859af2e4e7b1662ca0104e1db6a5d78971
kwj <kwj@vlax.net>
2024-09-12 14:25:46 -0700

    Display a header row before solver execution

--- python/euler/main.py
+++ python/euler/main.py
@@ -6,14 +6,13 @@ from time import perf_counter
 def main(nlst):
     for i in nlst:
         num = int(i)
+        print('[Problem {}]'.format(i))
         if (Path(__file__).parents[0] / 'bin' / 'p{:04d}.py'.format(num)).exists():
             module = import_module('euler.bin.p{:04d}'.format(num))
             start = perf_counter()
             result = module.solve()
             elapsed_time = perf_counter() - start
-            print('[Problem {}]'.format(i))
             print('Answer: {}'.format(result))
             print('Elapsed time: {:f} sec.\n'.format(elapsed_time))
         else:
-            print('[Problem {}]'.format(i))
             print('solver is not found.\n')
