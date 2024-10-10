83f3cc83b6aed711ef3f60e6a992ed7241e4fd41
kwj <kwj@vlax.net>
2024-10-10 09:15:07 -0700

    Remove an unnecessary variable

--- python/euler/bin/p0021.py
+++ python/euler/bin/p0021.py
@@ -4,8 +4,7 @@ from euler.lib.util import aliquot_sum_tbl
 
 
 def compute(num: int) -> str:
-    upper = num - 1
-    tbl = aliquot_sum_tbl(upper)
+    tbl = aliquot_sum_tbl(num - 1)
 
     return str(
         sum(x + tbl[x] for x in range(2, len(tbl)) if x > tbl[x] and tbl[tbl[x]] == x)
