974a8c9ad178d71af3cd7fe16e2cb60c34172cda
kwj <kwj@vlax.net>
2024-11-13 09:57:14 -0800

    Tweak

--- python/euler/bin/p0060.py
+++ python/euler/bin/p0060.py
@@ -41,7 +41,7 @@ from euler.lib.prime import is_prime, prime_generator
 from euler.lib.util import num_of_digits
 
 
-def get_pairable_primes(x: int, asc_ps: list[int], limit: int) -> list[int]:
+def get_pairable_primes(x: int, asc_ps: list[int], curr_minsum: int) -> list[int]:
     def fermat_primality_test(n: int) -> bool:
         # Note: 'n' must be an odd number.
         return pow(2, n - 1, n) == 1
@@ -56,7 +56,7 @@ def get_pairable_primes(x: int, asc_ps: list[int], limit: int) -> list[int]:
     upper_p = 10
     result = []
     for p in asc_ps:
-        if x + p >= limit:
+        if x + p >= curr_minsum:
             break
         while p > upper_p:
             upper_p *= 10
@@ -67,9 +67,9 @@ def get_pairable_primes(x: int, asc_ps: list[int], limit: int) -> list[int]:
 
 
 def find_cliques(
-    p: int, asc_ps: list[int], size: int, tbl: dict[int, set[int]]
+    p: int, asc_nbrs: list[int], size: int, tbl: dict[int, set[int]]
 ) -> list[list[int]]:
-    def check_concatenating(hd: int, tl: list[int]) -> bool:
+    def is_clique(hd: int, tl: list[int]) -> bool:
         if len(tl) == 0:
             return True
         else:
@@ -77,20 +77,21 @@ def find_cliques(
                 is_prime(int(str(hd) + str(x))) and is_prime(int(str(x) + str(hd)))
                 for x in tl
             ):
-                return check_concatenating(tl[0], tl[1:])
+                return is_clique(tl[0], tl[1:])
             else:
                 return False
 
-    def aux(group: list[int], desc_ps: list[int], depth: int) -> None:
-        if depth == 0 and check_concatenating(p, group) is True:
-            result.append(group)
+    def aux(group: list[int], desc_nbrs: list[int], depth: int) -> None:
+        if depth == 0:
+            if is_clique(p, group) is True:
+                result.append([p] + group)
         else:
-            for offset in range(0, len(desc_ps) - depth + 1):
-                if all(desc_ps[offset] in tbl[x] for x in group):
-                    aux(group + [desc_ps[offset]], desc_ps[offset + 1 :], depth - 1)
+            for offset in range(0, len(desc_nbrs) - depth + 1):
+                if all(desc_nbrs[offset] in tbl[x] for x in group):
+                    aux(group + [desc_nbrs[offset]], desc_nbrs[offset + 1 :], depth - 1)
 
     result: list[list[int]] = []
-    aux([], list(reversed(asc_ps)), size)
+    aux([], list(reversed(asc_nbrs)), size)
 
     return result
 
@@ -114,7 +115,7 @@ def compute(group_size: int) -> str:
 
         cliques = find_cliques(prime, nbrs, size, tbl)
         if len(cliques) > 0:
-            answer = min(answer, min(map(lambda x: prime + sum(x), cliques)))
+            answer = min(answer, min(map(sum, cliques)))
 
     return str(answer)
 
