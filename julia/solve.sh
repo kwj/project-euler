17260e631100525030d806b937a21186ce8d9b08
kwj <kwj@vlax.net>
2024-12-10 16:42:56 -0800

    Add a completion message at initialization

--- julia/solve.sh
+++ julia/solve.sh
@@ -3,7 +3,7 @@
 base_dir=$(cd $(dirname $0); pwd)
 
 if [ $# -eq 1 -a "$1" = "init" ]; then
-    julia --project=${base_dir}/Euler --eval 'using Pkg; Pkg.instantiate()'
+    julia --project=${base_dir}/Euler --eval 'using Pkg; Pkg.instantiate()' && echo 'Done'
 else
     julia --project=${base_dir}/Euler  --interactive --eval 'using Euler'
 fi
