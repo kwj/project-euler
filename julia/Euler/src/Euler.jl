06ec3300c6ad2c85f4e1d42be8d504e956d3bb43
kwj <kwj@vlax.net>
2025-08-11 20:03:38 -0700

    Tweak

--- julia/Euler/src/Euler.jl
+++ julia/Euler/src/Euler.jl
@@ -5,8 +5,9 @@ module Euler
 include.(filter(contains(r".jl$"), readdir(joinpath(@__DIR__, "lib"), join=true)))
 
 # read solutions
-for bin_dir in filter(x -> match(r"bin.*", x) !== nothing,readdir(@__DIR__))
+for bin_dir in filter(x -> match(r"bin.*", x) !== nothing, readdir(@__DIR__))
     include.(filter(contains(r".jl$"), readdir(joinpath(@__DIR__, bin_dir), join=true)))
 end
+
 
 end # module Euler
