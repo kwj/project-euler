c41ec8d24ea3dc97e56636df7575c2da46b630e5
kwj <kwj@vlax.net>
2026-05-18 12:19:45 -0700

    Rename variables

--- clojure/src/project_euler/sol/p0087.clj
+++ clojure/src/project_euler/sol/p0087.clj
@@ -12,13 +12,13 @@
          z4s (sequence (comp (map #(pow % 4)) (take-while #(< % limit))) ps)
          y3s (sequence (comp (map #(pow % 3)) (take-while #(< % limit))) ps)
          x2s (map #(pow % 2) ps)
-         sets (java.util.HashSet.)]
-     (doseq [sums (for [z4 z4s
-                        y3 y3s
-                        :let [tmp (+ z4 y3)]
-                        :while (< tmp limit)
-                        x2 x2s
-                        :while (< x2 (- limit tmp))]
-                    (+ tmp x2))]
-       (.add sets sums))
-     (.size sets))))
+         hash-set (java.util.HashSet.)]
+     (doseq [sum (for [z4 z4s
+                       y3 y3s
+                       :let [tmp (+ z4 y3)]
+                       :while (< tmp limit)
+                       x2 x2s
+                       :while (< x2 (- limit tmp))]
+                   (+ tmp x2))]
+       (.add hash-set sum))
+     (.size hash-set))))
