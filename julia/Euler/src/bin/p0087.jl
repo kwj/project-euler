df9a3b26e31a1edf5d516349a1032c3cfe00e0a5
kwj <kwj@vlax.net>
2026-05-18 16:50:15 -0700

    Make code simpler

--- julia/Euler/src/bin/p0087.jl
+++ julia/Euler/src/bin/p0087.jl
@@ -15,20 +15,19 @@ module Prob0087
 import Primes: primes
 
 function solve_0087(thr::Int = 50_000_000)
-    sq_plst = primes(isqrt(thr))
-    cb_plst = filter(x -> x^3 <= thr, sq_plst)
-    tsst_plst = filter(x -> x^4 <= thr, sq_plst)
+    p_lst = primes(isqrt(thr))
+    sq_plst = filter(x -> x < thr, map(x -> x^2, p_lst))
+    cb_plst = filter(x -> x < thr, map(x -> x^3, p_lst))
+    qu_plst = filter(x -> x < thr, map(x -> x^4, p_lst))
 
     result = Set{Int}()
-    for z in tsst_plst
-        for y in cb_plst
-            if (tmp = z^4 + y^3) >= thr
-                break
-            end
-            for x in sq_plst
-                if tmp + x^2 < thr
-                    push!(result, tmp + x^2)
-                end
+    for z⁴ in qu_plst
+        for y³ in cb_plst
+            (tmp = z⁴ + y³) >= thr && break
+            for x² in sq_plst
+                tmp + x² >= thr && break
+
+                push!(result, tmp + x²)
             end
         end
     end
