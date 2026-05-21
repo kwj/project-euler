bed09d20c7d7416a08af38ca106a68aed274d929
kwj <kwj@vlax.net>
2026-05-21 09:09:45 -0700

    Improve readability

--- clojure/src/project_euler/sol/p0043.clj
+++ clojure/src/project_euler/sol/p0043.clj
@@ -3,19 +3,18 @@
 
 (defn solve
   []
-  (let [candidates (loop [ds [1 1 17 13 11 7 5 3 2 1]
-                          lst '("")]
-                     (if (seq ds)
-                       (let [d (first ds)]
-                         (recur (rest ds)
-                                (for [x ["0" "1" "2" "3" "4" "5" "6" "7" "8" "9"]
-                                      s lst
-                                      :let [tmp-s (str x s)]
-                                      :when (and (not (str/includes? s x))
-                                                 (or (< (count tmp-s) 3)
-                                                     (zero? (mod (parse-long (subs tmp-s 0 3)) d))))]
-                                  tmp-s)))
-                       lst))
+  (let [divisors [1 1 17 13 11 7 5 3 2 1]
         xf (comp (filter #(not (= "0" (subs % 0 1))))
                  (map parse-long))]
-    (transduce xf + candidates)))
+    (letfn [(find-numbers [ds str-numbers]
+              (if-let [d (first ds)]
+                (recur (next ds)
+                       (for [x ["0" "1" "2" "3" "4" "5" "6" "7" "8" "9"]
+                             s str-numbers
+                             :when (not (str/includes? s x))
+                             :let [next-s (str x s)]
+                             :when (or (< (count next-s) 3)
+                                       (zero? (mod (parse-long (subs next-s 0 3)) d)))]
+                         next-s))
+                str-numbers))]
+      (transduce xf + (find-numbers divisors '(""))))))
