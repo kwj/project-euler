cab9f5e2dc94f6ce5efb93d8f12c3470bd522404
kwj <kwj@vlax.net>
2026-07-03 00:31:02 -0700

    Improve performance
    It became 10x faster on my Raspberry Pi 4. (25 sec. -> 2 sec.)

--- ocaml/src/p0060.ml
+++ ocaml/src/p0060.ml
@@ -1,69 +1,78 @@
 (* Project Euler: Problem 60 *)
 
-(*
- * This solution is slow. Here is a result on Raspberry Pi 4 Model B.
- *
- * % ./solve.sh 60
- * [Problem 60]
- * Answer: 26033
- * Elapsed time: 27.907s
- *)
-
 open Core
+module M = Euler.Math
 
-let is_pair x y =
-  let concat a b =
-    let rec aux n = if b > n then aux (n * 10) else n in
-    (a * aux 10) + b
+let get_pairable_primes x desc_ps curr_minsum =
+  let upper_x = M.int_pow 10 (M.num_of_digits x) in
+  let check_pair p upper_p =
+    M.Prime.(is_prime ((x * upper_p) + p) && is_prime ((p * upper_x) + x))
   in
-  Euler.Math.Prime.(is_prime (concat x y) && is_prime (concat y x))
+  let rec get_under_p p under_p =
+    if p > under_p then under_p else get_under_p p (under_p / 10)
+  in
+  let rec aux result under_p = function
+    | [] -> List.rev result
+    | p :: ps ->
+      if x + p >= curr_minsum
+      then aux result under_p ps
+      else (
+        let new_under_p = get_under_p p under_p in
+        match check_pair p (new_under_p * 10) with
+        | true -> aux (p :: result) new_under_p ps
+        | _ -> aux result new_under_p ps)
+  in
+  aux [] (M.int_pow 10 (M.get_max_exp (List.hd_exn desc_ps))) desc_ps
 ;;
 
-let find_nbrs p p_lst limit = List.filter p_lst ~f:(fun n -> n + p < limit && is_pair n p)
-
-let is_clique p_grp tbl =
-  let rec is_sublist a b =
-    match (a, b) with
-    | [], [] -> true
-    | [], _ -> true
-    | _, [] -> false
-    | x :: xs, y :: ys when x = y -> is_sublist xs ys
-    | _ :: _, _ :: ys -> is_sublist a ys
+let find_cliques p desc_nbr_lst size tbl =
+  let result = ref [] in
+  let rec aux group nbrs depth =
+    if depth = 0
+    then result := (p :: group) :: !result
+    else
+      for offset = 0 to List.length nbrs - depth do
+        let elem = List.nth_exn nbrs offset in
+        if List.for_all group ~f:(fun x -> Set.mem (Hashtbl.find_exn tbl x) elem)
+        then (
+          let _, rest_nbrs = List.split_n nbrs offset in
+          aux (elem :: group) rest_nbrs (pred depth))
+      done
   in
-  let rec loop = function
-    | [] -> true
-    | _ :: [] -> true
-    | x :: xs -> if is_sublist xs (Hashtbl.find_exn tbl x) then loop xs else false
-  in
-  loop p_grp
+  aux [] desc_nbr_lst size;
+  !result
 ;;
 
 let compute size_of_clique =
+  let size = size_of_clique - 1 in
   let prime_lst = [| ref [ 3 ]; ref [ 3 ] |] in
   let tbl = Hashtbl.create (module Int) in
+  Hashtbl.set tbl ~key:3 ~data:Int.Set.empty;
 
-  let rec loop p ans =
-    let p = Euler.Math.Prime.next_prime p in
-    if p >= ans
-    then ans
+  let rec loop prev_p answer =
+    let p = M.Prime.next_prime prev_p in
+
+    if p >= answer
+    then answer
     else (
       let idx = (p + 2) mod 3 in
-      let nbr_lst = find_nbrs p !(prime_lst.(idx)) ans in
-      Hashtbl.set tbl ~key:p ~data:nbr_lst;
+      let desc_nbr_lst = get_pairable_primes p !(prime_lst.(idx)) answer in
+      Hashtbl.set tbl ~key:p ~data:(Int.Set.of_list desc_nbr_lst);
       prime_lst.(idx) := p :: !(prime_lst.(idx));
 
-      if List.length nbr_lst < size_of_clique - 1
-      then loop p ans
+      if List.length desc_nbr_lst < size_of_clique - 1
+      then loop p answer
       else (
-        let rec aux ans = function
-          | [] -> ans
-          | p_grp :: xs ->
-            let tmp = p + List.reduce_exn p_grp ~f:( + ) in
-            if tmp < ans && is_clique p_grp tbl
-            then aux (Int.min tmp ans) xs
-            else aux ans xs
-        in
-        loop p (aux ans (Euler.Util.combination (size_of_clique - 1) nbr_lst))))
+        match find_cliques p desc_nbr_lst size tbl with
+        | [] -> loop p answer
+        | cliques ->
+          let min_sum_clique =
+            List.(
+              map cliques ~f:(reduce_exn ~f:( + ))
+              |> min_elt ~compare:Int.compare
+              |> Option.value_exn)
+          in
+          loop p (min answer min_sum_clique)))
   in
   loop 5 Int.max_value
 ;;
