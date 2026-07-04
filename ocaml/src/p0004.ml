d86c318df7cf032e31558e3b953a443afeaf7063
kwj <kwj@vlax.net>
2026-07-04 13:43:06 -0700

    Add a test case

--- ocaml/src/p0004.ml
+++ ocaml/src/p0004.ml
@@ -3,9 +3,9 @@
 open Core
 
 let make_segments low high =
-  let blk_size = low * low in
+  let blk_size = if low = 0 then 1 else low * low in
   List.(
-    range blk_size (high * high) ~stride:blk_size ~stop:`inclusive
+    range (low * low) (high * high) ~stride:blk_size ~stop:`inclusive
     |> map ~f:(fun x -> (x, x + blk_size - 1))
     |> rev)
   |> Sequence.of_list
@@ -13,7 +13,10 @@ let make_segments low high =
 
 let find_max_palindrome_number low high (blk_low, blk_high) =
   Sequence.(
-    range (Euler.Math.isqrt blk_low) (min high (blk_high / low)) ~stop:`inclusive
+    range
+      (Euler.Math.isqrt blk_low)
+      (if low = 0 then high else min high (blk_high / low))
+      ~stop:`inclusive
     |> concat_map ~f:(fun x ->
       range x low ~stride:(-1) ~stop:`inclusive
       |> map ~f:(( * ) x)
@@ -25,7 +28,7 @@ let find_max_palindrome_number low high (blk_low, blk_high) =
 
 let compute n_digits =
   assert (n_digits > 0);
-  let low = Int.pow 10 (n_digits - 1) in
+  let low = if n_digits = 1 then 0 else Int.pow 10 (n_digits - 1) in
   let high = Int.pow 10 n_digits - 1 in
 
   make_segments low high
@@ -37,5 +40,6 @@ let solve () = compute 3 |> Int.to_string
 
 (* Test *)
 
-let%test_unit "2-digit numbers" = [%test_eq: int] (compute 2) 9009
-let%test_unit "3-digit numbers" = [%test_eq: int] (compute 3) 906609
+let%test_unit "product of two 1-digit numbers" = [%test_eq: int] (compute 1) 9
+let%test_unit "product of two 2-digit numbers" = [%test_eq: int] (compute 2) 9009
+let%test_unit "product of two 3-digit numbers" = [%test_eq: int] (compute 3) 906609
