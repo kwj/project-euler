296ff3eeed04332a1dffd4bb3f5441d9180ce159
kwj <kwj@vlax.net>
2026-07-03 18:46:37 -0700

    Improve performance

--- ocaml/src/p0004.ml
+++ ocaml/src/p0004.ml
@@ -2,17 +2,33 @@
 
 open Core
 
+let make_segments low high =
+  let blk_size = low * low in
+  List.(
+    range blk_size (high * high) ~stride:blk_size ~stop:`exclusive
+    |> map ~f:(fun x -> (x, x + blk_size - 1))
+    |> rev)
+  |> Sequence.of_list
+;;
+
+let find_max_palindrome_number low high (blk_low, blk_high) =
+  Sequence.(
+    range (Euler.Math.isqrt blk_low) (min high (blk_high / low)) ~stop:`inclusive
+    |> concat_map ~f:(fun x ->
+      range x low ~stride:(-1) ~stop:`inclusive
+      |> map ~f:(( * ) x)
+      |> take_while ~f:(( <= ) blk_low)
+      |> filter ~f:Euler.Math.is_palindrome)
+    |> max_elt ~compare:Int.compare)
+;;
+
 let compute n_digits =
-  let lst = ref [] in
   let low = Int.pow 10 (n_digits - 1) in
   let high = Int.pow 10 n_digits - 1 in
-  for x = low to high do
-    for y = x to high do
-      let tmp = x * y in
-      if Euler.Math.is_palindrome tmp then lst := tmp :: !lst
-    done
-  done;
-  List.sort ~compare:(fun x y -> y - x) !lst |> List.hd_exn
+
+  make_segments low high
+  |> Sequence.find_map ~f:(find_max_palindrome_number low high)
+  |> Option.value_exn
 ;;
 
 let solve () = compute 3 |> Int.to_string
