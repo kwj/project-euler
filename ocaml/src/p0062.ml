8e2ca662609443121637162a45c5436142db1d1d
kwj <kwj@vlax.net>
2026-07-05 11:26:29 -0700

    Tweak

--- ocaml/src/p0062.ml
+++ ocaml/src/p0062.ml
@@ -3,8 +3,9 @@
 open Core
 
 let compute qty =
-  let make_key n = Euler.Util.digits n |> List.sort ~compare |> Euler.Util.undigits in
+  let make_key n = Euler.Util.digits n |> List.sort ~compare:Int.compare |> Euler.Util.undigits in
   let tbl = Hashtbl.create (module Int) in
+
   let rec loop n =
     let cube = Int.pow n 3 in
     let cube_lst =
