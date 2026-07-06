4d5675a24438a94e70bd7f206ab4bdca9c41c594
kwj <kwj@vlax.net>
2026-07-05 21:42:24 -0700

    Tweak

--- ocaml/src/p0049.ml
+++ ocaml/src/p0049.ml
@@ -2,6 +2,8 @@
 
 open Core
 
+let no_exist_msg = "There are no such sequences."
+
 let get_prime_group_lst n_digits =
   let make_key n =
     Euler.Util.(digits n |> List.sort ~compare:Int.ascending |> undigits)
@@ -16,26 +18,28 @@ let get_prime_group_lst n_digits =
 let compute n_digits =
   assert (n_digits > 0 && n_digits <= 4);
 
-  List.(
-    get_prime_group_lst n_digits
-    |> filter_map ~f:(fun lst ->
-      if length lst < 3 then None else Some (sort ~compare:Int.ascending lst))
-    >>= Euler.Util.combination 3
-    |> filter ~f:(fun lst ->
-      nth_exn lst 1 - nth_exn lst 0 = nth_exn lst 2 - nth_exn lst 1)
-    |> map ~f:(Fun.compose (reduce_exn ~f:( ^ )) (map ~f:Int.to_string))
-    |> filter ~f:(fun s -> String.compare s "148748178147" <> 0)
-    |> hd
-    |> function
-    | None -> "There are no such sequences."
-    | Some s -> s)
+  let result =
+    List.(
+      get_prime_group_lst n_digits
+      |> filter_map ~f:(fun lst ->
+        if length lst < 3 then None else Some (sort ~compare:Int.ascending lst))
+      >>= Euler.Util.combination 3
+      |> filter ~f:(fun lst ->
+        nth_exn lst 1 - nth_exn lst 0 = nth_exn lst 2 - nth_exn lst 1)
+      |> map ~f:(Fun.compose (reduce_exn ~f:( ^ )) (map ~f:Int.to_string))
+      |> filter ~f:(Fun.compose not (String.equal "148748178147")))
+  in
+  match List.length result with
+  | 0 -> no_exist_msg
+  | 1 -> List.hd_exn result
+  | _ -> failwith "Error: There are two or more sequences."
 ;;
 
 let solve () = compute 4
 
 (* Test *)
 
-let%test_unit "1" = [%test_eq: string] (compute 1) "There are no such sequences."
-let%test_unit "2" = [%test_eq: string] (compute 2) "There are no such sequences."
-let%test_unit "3" = [%test_eq: string] (compute 3) "There are no such sequences."
+let%test_unit "1" = [%test_eq: string] (compute 1) no_exist_msg
+let%test_unit "2" = [%test_eq: string] (compute 2) no_exist_msg
+let%test_unit "3" = [%test_eq: string] (compute 3) no_exist_msg
 let%test_unit "4" = [%test_eq: string] (compute 4) "296962999629"
