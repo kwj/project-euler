0842f31ab68298ded6f209c184bbd213d95b3f25
kwj <kwj@vlax.net>
2026-07-05 18:14:32 -0700

    Make code simpler

--- ocaml/src/p0049.ml
+++ ocaml/src/p0049.ml
@@ -2,35 +2,31 @@
 
 open Core
 
-let get_prime_tbl n_digits =
+let get_prime_group_lst n_digits =
   let make_key n = Euler.Util.(digits n |> List.sort ~compare:Int.compare |> undigits) in
 
   Euler.Math.Prime.primes (Int.pow 10 (n_digits - 1)) (Int.pow 10 n_digits)
-  |> List.filter_map ~f:(fun n -> if n >= 1000 then Some (make_key n, n) else None)
-  |> Hashtbl.of_alist_multi ~growth_allowed:false (module Int)
+  |> List.map ~f:(fun n -> (make_key n, n))
+  |> Euler.Util.list_assoc_group
+  |> List.map ~f:snd
 ;;
 
 let compute n_digits =
+  assert (n_digits > 0);
+
   List.(
-    get_prime_tbl n_digits
-    |> Hashtbl.to_alist
-    |> filter_map ~f:(fun (_, lst) ->
-      if length lst < 3
-      then None
-      else (
-        let cands =
-          Euler.Util.combination 2 lst
-          |> filter_map ~f:(fun l ->
-            let tmp = (nth_exn l 0 * 2) - nth_exn l 1 in
-            if exists lst ~f:(fun n -> n = tmp)
-            then Some [ tmp; nth_exn l 0; nth_exn l 1 ]
-            else None)
-        in
-        if length cands > 0 then Some cands else None))
-    |> concat
-    |> map ~f:(Fun.compose (reduce_exn ~f:( ^ )) (rev_map ~f:Int.to_string))
+    get_prime_group_lst n_digits
+    |> filter_map ~f:(fun lst ->
+      if length lst < 3 then None else Some (sort ~compare:Int.ascending lst))
+    >>= Euler.Util.combination 3
+    |> filter ~f:(fun lst ->
+      nth_exn lst 1 - nth_exn lst 0 = nth_exn lst 2 - nth_exn lst 1)
+    |> map ~f:(Fun.compose (reduce_exn ~f:( ^ )) (map ~f:Int.to_string))
     |> filter ~f:(fun s -> String.compare s "148748178147" <> 0)
-    |> hd_exn)
+    |> hd
+    |> function
+    | None -> "There are no such sequences."
+    | Some s -> s)
 ;;
 
 let solve () = compute 4
