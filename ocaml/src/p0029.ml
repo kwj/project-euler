b6aed79525daf236657ec4377d187f8438e608a9
kwj <kwj@vlax.net>
2026-07-06 17:17:23 -0700

    Remove unnecessary to_list

--- ocaml/src/p0029.ml
+++ ocaml/src/p0029.ml
@@ -5,16 +5,17 @@ open Core
 let make_dupctr_tbl upper =
   let max_exp = Euler.Math.get_max_exp upper ~base:2 in
   let tbl = Array.create 0 ~len:(max_exp + 1) in
+  let dups = Array.create 0 ~len:(upper + 1) in
   Sequence.(
     range 2 max_exp ~stop:`inclusive
     |> iter ~f:(fun x ->
-      let dups = Array.create 0 ~len:(upper + 1) in
+      Array.fill dups ~pos:0 ~len:(Array.length dups) 0;
       range 1 x ~stop:`exclusive
       |> iter ~f:(fun y ->
         let k = Euler.Math.lcm x y / x in
         range (max k 2) (upper * y / x) ~stop:`inclusive ~stride:k
         |> iter ~f:(fun idx -> dups.(idx) <- 1));
-      tbl.(x) <- Array.to_list dups |> List.reduce_exn ~f:( + )));
+      tbl.(x) <- Array.reduce_exn dups ~f:( + )));
   tbl
 ;;
 
